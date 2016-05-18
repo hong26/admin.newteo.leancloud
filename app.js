08e74196cf0836fe1af0703c859ad1c7f7f88871
joephon <306519779@qq.com>
2016-05-18 12:00:50 +0800

    init

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var todos = require('./routes/todos');
 var cloud = require('./cloud');
 
 var app = express();
@@ -50,8 +49,6 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// 可以将一类的路由单独保存在一个文件中
-app.use('/todos', todos);
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
